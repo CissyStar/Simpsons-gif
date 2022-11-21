bde8ea188087598a13719b01e82867f137de09ad
Cissy Star <102735540+CissyStar@users.noreply.github.com>
2022-11-21 20:25:03 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
 const giphyApiKey = "3Bqe6VoIAUhdUAAl6FcwPNGowW9nGU0m";
 const output = document.querySelector("#output");
 
-function getSimpsonsQuote() {
-  fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res[0].character);
-      return res[0].character;
-    })
-    .catch((error) => console.log(`Giphy API request error: ${error}`));
+async function getSimpsonsQuote() {
+  try {
+    const simpsonsPromise = await fetch(
+      "https://thesimpsonsquoteapi.glitch.me/quotes"
+    );
+    const simpsonsJson = await simpsonsPromise.json();
+
+    return simpsonsJson[0].character;
+  } catch (error) {
+    console.log(`Giphy API request error: ${error}`);
+  }
 }
 
-function getGiphyApi(character) {
-  fetch(
-    "https://api.giphy.com/v1/gifs/search?" +
-      new URLSearchParams({ api_key: giphyApiKey, q: character })
-  )
-    .then((res) => res.json())
-    .then((res) => {
-      const randomNum = Math.floor(Math.random() * res.data.length);
-      console.log(res.data[randomNum].images.original.url);
-      return res.data[randomNum].images.original.url;
-    })
-    .catch((error) => console.log(`Giphy API request error: ${error}`));
+async function getGiphyApi(character) {
+  try {
+    const giphyPromise = await fetch(
+      "https://api.giphy.com/v1/gifs/search?" +
+        new URLSearchParams({ api_key: giphyApiKey, q: character })
+    );
+    const giphyJson = await giphyPromise.json();
+    const randomNum = Math.floor(Math.random() * giphyJson.data.length);
+
+    return giphyJson.data[randomNum].images.original.url;
+  } catch (error) {
+    console.log(`Giphy API request error: ${error}`);
+  }
 }
 
-async function getGif() {
-  const character = await getSimpsonsQuote();
-  const url = await getGiphyApi(character);
-  output.innerHTML += `<img src=${url} />`;
+async function initGif() {
+  try {
+    let searchCharacter = await getSimpsonsQuote();
+    output.innerHTML += `<h1>${searchCharacter}</h1>`;
+    let gifUrl = await getGiphyApi(searchCharacter);
+    output.innerHTML += `<img src=${gifUrl}/>`;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-getGif();
+initGif();
